4b949cefd338c05d539630ebfc5e3f535977b2dc
RepentMF <32201016+RepentMF@users.noreply.github.com>
2019-11-23 12:03:59 -0500

    Added attack animations
    up down left right, added states

--- UtFoM_Project2/Assets/Scripts/PlayerMovement.cs
+++ UtFoM_Project2/Assets/Scripts/PlayerMovement.cs
@@ -2,8 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerState
+{
+	walk,
+	attack,
+	interact
+}
+
 public class PlayerMovement : MonoBehaviour
 {
+	public PlayerState currentState;
 	public float speed;
 	private Rigidbody2D myRigidbody;
 	private Vector3 change;
@@ -17,6 +25,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+		currentState = PlayerState.walk;
 		animator = GetComponent<Animator>();
 		myRigidbody = GetComponent<Rigidbody2D>();
     }
@@ -27,8 +36,25 @@ public class PlayerMovement : MonoBehaviour
 		change = Vector2.zero;
 		change.x = Input.GetAxisRaw("Horizontal");
 		change.y = Input.GetAxisRaw("Vertical");
-		UpdateAnimationAndMove();
+		if (Input.GetButtonDown("attack") && currentState != PlayerState.attack) 
+		{
+			StartCoroutine (AttackCo ());
+		}
+		else if (currentState == PlayerState.walk) 
+		{
+			UpdateAnimationAndMove ();
+		}
     }
+
+	private IEnumerator AttackCo()
+	{
+		animator.SetBool("attacking", true);
+		currentState = PlayerState.attack;
+		yield return null;
+		animator.SetBool("attacking", false);
+		yield return new WaitForSeconds(.3f);
+		currentState = PlayerState.walk;
+	}
 
 	void UpdateAnimationAndMove()
 	{
