68ee85623a1a23e968e480db08fd5975b16cf791
RepentMF <32201016+RepentMF@users.noreply.github.com>
2020-12-07 19:15:26 -0500

    changed playermovement to be simplified

--- UtFoM_Project2/Assets/Scripts/PlayerMovement.cs
+++ UtFoM_Project2/Assets/Scripts/PlayerMovement.cs
@@ -17,17 +17,11 @@ public class PlayerMovement : MonoBehaviour
 	public PlayerState currentState;
 	public float speed;
 	private Rigidbody2D rigidbody;
-	private Vector3 change;
+	public Vector3 change;
 	private Animator animator;
 	public FloatValue currentHealth;
 	public Signal playerHealthSignal;
 	public VectorValue startingPosition;
-
-	void MoveCharacter()
-	{
-		change.Normalize();
-		rigidbody.MovePosition(transform.position + change * speed * Time.deltaTime);
-	}
 
 	private IEnumerator KnockCo(float kbTime)
 	{
@@ -55,45 +49,10 @@ public class PlayerMovement : MonoBehaviour
 		}
 	}
 
-	private IEnumerator KnifeAttackCo()
-	{
-		animator.SetBool("knifing", true);
-		currentState = PlayerState.knife;
-		yield return null;
-		animator.SetBool("knifing", false);
-		yield return new WaitForSeconds(.08f);
-		currentState = PlayerState.idle;
-	}
-
-	private IEnumerator HammerAttackCo()
-	{
-		animator.SetBool("hammering", true);
-		currentState = PlayerState.hammer1;
-		yield return null;
-		animator.SetBool("hammering", false);
-		yield return new WaitForSeconds(0.83f);
-		currentState = PlayerState.idle;
-	}
-
-	void UpdateAnimationAndMove()
-	{
-		if (change != Vector3.zero)
-		{
-			MoveCharacter();
-			animator.SetFloat("moveX", change.x);
-			animator.SetFloat("moveY", change.y);
-			animator.SetBool("moving", true);
-		}
-		else
-		{
-			animator.SetBool("moving", false);
-		}
-	}
-
     // Start is called before the first frame update
     void Start()
     {
-		currentState = PlayerState.idle;
+		//currentState = PlayerState.idle;
 		animator = GetComponent<Animator>();
 		rigidbody = GetComponent<Rigidbody2D>();
 		animator.SetFloat("moveX", 0);
@@ -104,22 +63,42 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		change = Vector2.zero;
 		change.x = Input.GetAxisRaw("Horizontal");
 		change.y = Input.GetAxisRaw("Vertical");
-		if (Input.GetButtonDown("knife") && (currentState != PlayerState.knife || currentState != PlayerState.hammer1) &&
-			currentState != PlayerState.stagger) 
+
+		if (Input.GetButtonDown("knife"))
 		{
-			StartCoroutine(KnifeAttackCo());
+			animator.Play("Knifing");
+			animator.SetBool("moving", false);
 		}
-		else if (Input.GetButtonDown("hammer1") && (currentState != PlayerState.knife || currentState != PlayerState.hammer1) &&
-			currentState != PlayerState.stagger)
+		else if (Input.GetButtonDown("hammer1"))
 		{
-			StartCoroutine(HammerAttackCo());
+			animator.Play("Hammering");
+			animator.SetBool("moving", false);
 		}
-		else if (currentState == PlayerState.walk || currentState == PlayerState.idle) 
+		else if (change != Vector3.zero && !animator.GetCurrentAnimatorStateInfo(0).IsName("Knifing") && 
+			!animator.GetCurrentAnimatorStateInfo(0).IsName("Hammering"))
 		{
-			UpdateAnimationAndMove();
+			animator.SetFloat("moveX", change.x);
+			animator.SetFloat("moveY", change.y);
+			animator.SetBool("moving", true);
 		}
+		else if (change == Vector3.zero)
+		{
+			animator.SetBool("moving", false);
+		}
+    }
+
+    void FixedUpdate()
+    {
+    	if (animator.GetBool("moving"))
+    	{
+			change.Normalize();
+			rigidbody.MovePosition(transform.position + change * speed * Time.deltaTime);
+    	}
+    	else if (animator.GetCurrentAnimatorStateInfo(0).IsName("Knifing"))
+    	{
+    		change = Vector3.zero;
+    	}
     }
 }
