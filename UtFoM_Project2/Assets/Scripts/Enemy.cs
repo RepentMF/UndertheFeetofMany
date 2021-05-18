705a82671e96217218eb3e7083121184809bf92e
RepentMF <32201016+RepentMF@users.noreply.github.com>
2021-05-18 00:26:07 -0400

    editing map and enemies

--- UtFoM_Project2/Assets/Scripts/Enemy.cs
+++ UtFoM_Project2/Assets/Scripts/Enemy.cs
@@ -16,24 +16,93 @@ public enum EnemyState
 public class Enemy : MonoBehaviour
 {
 	public EnemyState currentState;
-	
 	public bool hit;
 	public int combo;
 	public int baseAtk;
+	public int baseWeight;
+	public int currentWeight;
 	public FloatValue maxHealth;
 	public FloatValue currentHealth;
 	public FloatValue currentStamina;
 	public float moveSpeed;
-	public float homePos;
+	public float home;
 	public float currentStagger;
 	public string enemyName;
 	public string hitBy;
 
+
+	public Rigidbody2D rigidbody;
+	public Transform target;
+	public Transform homePos;
+	public float chaseRadius;
+	public float attackRadius;
+	public Animator animator;
+
+	void CheckDistance()
+	{
+		if(Vector3.Distance(target.position, transform.position) <= chaseRadius && 
+			Vector3.Distance(target.position, transform.position) > attackRadius)
+		{
+			if(currentState == EnemyState.idle || currentState == EnemyState.walk &&
+				currentState != EnemyState.stagger)
+			{
+				Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+				rigidbody.MovePosition(temp);
+				ChangeAnim(temp - transform.position);
+				ChangeState(EnemyState.walk);
+				animator.SetBool("awake", true);
+			}
+		}
+		else if(Vector3.Distance(target.position, transform.position) > chaseRadius)
+		{
+			animator.SetBool("awake", false);
+		}
+	}
+
+	private void ChangeState(EnemyState newState)
+	{
+		if(currentState != newState)
+		{
+			currentState = newState;
+		}
+	}
+
+	private void SetAnimFloat(Vector2 vec)
+	{
+		animator.SetFloat("moveX",  vec.x);
+		animator.SetFloat("moveY", vec.y);
+	}
+
+	private void ChangeAnim(Vector2 dir)
+	{
+		if(Mathf.Abs(dir.x) > Mathf.Abs(dir.y))
+		{
+			if(dir.x > 0)
+			{
+				SetAnimFloat(Vector2.right);
+			}
+			else if(dir.x < 0)
+			{
+				SetAnimFloat(Vector2.left);
+			}
+		}
+		else if(Mathf.Abs(dir.x) < Mathf.Abs(dir.y))
+		{
+			if(dir.y > 0)
+			{
+				SetAnimFloat(Vector2.up);
+			}
+			else if(dir.y < 0)
+			{
+				SetAnimFloat(Vector2.down);
+			}
+		}
+	}
 	public void Knock(float damage, bool execute)
 	{
 		if(combo == 0)
 		{
-			homePos = transform.position.y;
+			home = transform.position.y;
 		}
 
 		if(!hit)
@@ -60,6 +129,13 @@ public class Enemy : MonoBehaviour
     	hit = false;
     	combo = 0;
     	currentStagger = 0f;
+    	currentWeight = baseWeight;
+
+
+		currentState = EnemyState.idle;
+		rigidbody = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
+		target = GameObject.FindWithTag("Player").transform;
     }
 
     // Update is called once per frame
@@ -93,7 +169,7 @@ public class Enemy : MonoBehaviour
 			}
     	}
 		
-		if(transform.position.y < homePos && currentState == EnemyState.freefall)
+		if(transform.position.y < home && currentState == EnemyState.freefall)
 		{
 			currentState = EnemyState.idle;
 		}
@@ -104,6 +180,7 @@ public class Enemy : MonoBehaviour
 			this.gameObject.SetActive(false);
 		}
 
+		CheckDistance();
 
 		//GetComponent<Rigidbody2D>().velocity = new Vector2 (3f, 0);
 		Debug.Log(currentStamina.runtimeValue);
--- UtFoM_Project2/Assets/Scripts/Log.cs
+++ UtFoM_Project2/Assets/Scripts/Log.cs
@@ -4,73 +4,13 @@ using UnityEngine;
 
 public class Log : Enemy
 {
-	private Rigidbody2D rigidbody;
-	public Transform target;
-	public Transform homePos;
-	public float chaseRadius;
-	public float attackRadius;
-	public Animator animator;
+	// public Transform target;
+	// public Transform homePos;
+	// public float chaseRadius;
+	// public float attackRadius;
+	// public Animator animator;
 
-	void CheckDistance()
-	{
-		if(Vector3.Distance(target.position, transform.position) <= chaseRadius && 
-			Vector3.Distance(target.position, transform.position) > attackRadius)
-		{
-			if(currentState == EnemyState.idle || currentState == EnemyState.walk &&
-				currentState != EnemyState.stagger)
-			{
-				Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
-				rigidbody.MovePosition(temp);
-				ChangeAnim(temp - transform.position);
-				ChangeState(EnemyState.walk);
-				animator.SetBool("awake", true);
-			}
-		}
-		else if(Vector3.Distance(target.position, transform.position) > chaseRadius)
-		{
-			animator.SetBool("awake", false);
-		}
-	}
-
-	private void ChangeState(EnemyState newState)
-	{
-		if(currentState != newState)
-		{
-			currentState = newState;
-		}
-	}
-
-	private void SetAnimFloat(Vector2 vec)
-	{
-		animator.SetFloat("moveX",  vec.x);
-		animator.SetFloat("moveY", vec.y);
-	}
-
-	private void ChangeAnim(Vector2 dir)
-	{
-		if(Mathf.Abs(dir.x) > Mathf.Abs(dir.y))
-		{
-			if(dir.x > 0)
-			{
-				SetAnimFloat(Vector2.right);
-			}
-			else if(dir.x < 0)
-			{
-				SetAnimFloat(Vector2.left);
-			}
-		}
-		else if(Mathf.Abs(dir.x) < Mathf.Abs(dir.y))
-		{
-			if(dir.y > 0)
-			{
-				SetAnimFloat(Vector2.up);
-			}
-			else if(dir.y < 0)
-			{
-				SetAnimFloat(Vector2.down);
-			}
-		}
-	}
+	
 
     // Start is called before the first frame update
     void Start()
@@ -89,6 +29,6 @@ public class Log : Enemy
 
     void FixedUpdate()
     {
-		CheckDistance();
+    	
     }
 }
