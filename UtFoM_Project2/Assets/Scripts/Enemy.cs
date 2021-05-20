28a190c39b3b6687bae58c3beff5226e42884974
RepentMF <32201016+RepentMF@users.noreply.github.com>
2021-05-20 09:12:09 -0400

    enemy logic

--- UtFoM_Project2/Assets/Scripts/Enemy.cs
+++ UtFoM_Project2/Assets/Scripts/Enemy.cs
@@ -40,40 +40,15 @@ public class Enemy : MonoBehaviour
 
 	void CheckDistance()
 	{
-		if(Vector3.Distance(target.position, transform.position) <= chaseRadius && 
-			Vector3.Distance(target.position, transform.position) > attackRadius)
-		{
-			if(currentState == EnemyState.idle || currentState == EnemyState.walk &&
-				currentState != EnemyState.stagger)
-			{
-				Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
-				rigidbody.MovePosition(temp);
-				ChangeAnim(temp - transform.position);
-				ChangeState(EnemyState.walk);
-				animator.SetBool("awake", true);
-			}
-		}
-		else if(Vector3.Distance(target.position, transform.position) > chaseRadius)
-		{
-			animator.SetBool("awake", false);
-		}
-	}
-
-	private void ChangeState(EnemyState newState)
-	{
-		if(currentState != newState)
-		{
-			currentState = newState;
-		}
 	}
 
 	private void SetAnimFloat(Vector2 vec)
 	{
-		animator.SetFloat("moveX",  vec.x);
+		animator.SetFloat("moveX", vec.x);
 		animator.SetFloat("moveY", vec.y);
 	}
 
-	private void ChangeAnim(Vector2 dir)
+	private void ChangeDir(Vector2 dir)
 	{
 		if(Mathf.Abs(dir.x) > Mathf.Abs(dir.y))
 		{
@@ -98,6 +73,7 @@ public class Enemy : MonoBehaviour
 			}
 		}
 	}
+
 	public void Knock(float damage, bool execute)
 	{
 		if(combo == 0)
@@ -181,7 +157,14 @@ public class Enemy : MonoBehaviour
 		}
 
 		CheckDistance();
-
+		if(currentState == EnemyState.stagger || currentState == EnemyState.juggle)
+		{
+			animator.Play("stagger");
+		}
+		else if(currentState == EnemyState.idle)
+		{
+			animator.Play("idle");
+		}
 		//GetComponent<Rigidbody2D>().velocity = new Vector2 (3f, 0);
 		Debug.Log(currentStamina.runtimeValue);
     }
