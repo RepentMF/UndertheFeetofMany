c100bc3fc86032c4c4d040ea17e7541ac1b31f21
RepentMF <32201016+RepentMF@users.noreply.github.com>
2021-10-27 21:51:20 -0400

    this is really bad code help me

--- UtFoM_Project2/Assets/Scripts/RoomManager.cs
+++ UtFoM_Project2/Assets/Scripts/RoomManager.cs
@@ -5,17 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class RoomManager : MonoBehaviour
 {
-
+	private SceneManager sceneManager;
+	public List<List<bool>> listHolder;
+	public List<bool> itemHolders0;
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Break();
+    	Debug.Log("this is being logged");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    	// idk what i'm doing save me
+    	// for(int i = 0; i < 38; i++)
+    	// {
+    	// 	if(SceneManager.GetActiveScene().buildIndex == i)
+    	// 	{
+
+    	// 	}
+    	// }
     }
 }
