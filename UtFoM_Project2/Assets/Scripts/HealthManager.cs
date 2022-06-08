c3390d137308b474a9682a9fe980080cdf9c9bf8
RepentMF <32201016+RepentMF@users.noreply.github.com>
2022-06-08 16:17:19 -0400

    new cutscene stuff and added stat ui functionality

--- UtFoM_Project2/Assets/Scripts/HealthManager.cs
+++ UtFoM_Project2/Assets/Scripts/HealthManager.cs
@@ -5,16 +5,6 @@ using UnityEngine.UI;
 
 public class HealthManager : MonoBehaviour
 {
-	public float currentHealth;
-	public float maxHealth;
-	public float currentMana;
-	public float maxMana;
-	public float currentStamina;
-	public float maxStamina;
-	public float currentLifeblood;
-	public float maxLifeblood;
-	public float lifebloodDecimal;
-
 	public Image healthBar;
 	public Image manaBar;
 	public Image staminaBar;
@@ -22,39 +12,35 @@ public class HealthManager : MonoBehaviour
 	public Image lbFlare1;
 	public Image lbFlare2;
 	public Image lbFlare3;
-	public PlayerMovement player;
+	public Stats playerStats;
 	public Enemy enemy;
 
 	public void UpdatePlayerStats()
 	{
-		healthBar.fillAmount = player.currentHealth / player.maxHealth;
-		manaBar.fillAmount = player.currentMana / player.maxMana;
-		staminaBar.fillAmount = player.currentStamina / player.maxStamina;
+		healthBar.fillAmount = playerStats.CurrentHealth / playerStats.MaxHealth;
+		manaBar.fillAmount = playerStats.CurrentMana / playerStats.MaxMana;
+		staminaBar.fillAmount = playerStats.CurrentStamina / playerStats.MaxStamina;
 
-		if(player.currentLifeblood >= 0f && player.currentLifeblood <= 10f)
+		if(playerStats.CurrentLifeblood >= 0f && playerStats.CurrentLifeblood <= 10f)
 		{
-			lifebloodNotches.fillAmount = 0.032f + (0.0133f * player.currentLifeblood);
+			lbFlare3.fillAmount = playerStats.CurrentLifeblood / 10f;
 			lbFlare1.gameObject.SetActive(false);
 			lbFlare2.gameObject.SetActive(false);
-			lbFlare3.gameObject.SetActive(false);
+			lbFlare3.gameObject.SetActive(true);
 		}
-		else if(player.currentLifeblood > 10f && player.currentLifeblood <= 20f)
+		else if(playerStats.CurrentLifeblood > 10f && playerStats.CurrentLifeblood <= 20f)
 		{
-			lifebloodNotches.fillAmount = 0.434f + (0.0133f * (player.currentLifeblood - 10f));
-			lbFlare1.gameObject.SetActive(true);
-			lbFlare2.gameObject.SetActive(false);
-			lbFlare3.gameObject.SetActive(false);
+			lbFlare2.fillAmount = (playerStats.CurrentLifeblood - 10f) / 10f;
+			lbFlare1.gameObject.SetActive(false);
+			lbFlare2.gameObject.SetActive(true);
+			lbFlare3.gameObject.SetActive(true);
 		}
-		else if(player.currentLifeblood > 20f && player.currentLifeblood <= 30f)
+		else if(playerStats.CurrentLifeblood > 20f && playerStats.CurrentLifeblood <= 30f)
 		{
-			lifebloodNotches.fillAmount = 0.835f + (0.0133f * (player.currentLifeblood - 20f));
+			lbFlare1.fillAmount = (playerStats.CurrentLifeblood - 20f) / 10f;
 			lbFlare1.gameObject.SetActive(true);
 			lbFlare2.gameObject.SetActive(true);
-			lbFlare3.gameObject.SetActive(false);
-			if(player.currentLifeblood == 30f)
-			{
-				lbFlare3.gameObject.SetActive(true);
-			}
+			lbFlare3.gameObject.SetActive(true);
 		}
 	}
 
