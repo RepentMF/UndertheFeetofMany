41634bf5f0a0293d44b24fabe0dfecd6082fd939
RepentMF <32201016+RepentMF@users.noreply.github.com>
2022-06-16 17:37:07 -0400

    editing maps

---
+++ UtFoM_Project2/Assets/Scripts/IndoorFader.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndoorFader : MonoBehaviour
+{
+    public float FadeSpeed = 1f;
+    private bool ShouldFadeObject = false;
+
+    // Script References
+    private SpriteRenderer SpriteRendererScript;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        // If a player has entered the object's space
+        if (collider.GetComponent<PlayerController>() != null)
+        {
+            ShouldFadeObject = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        // If a player has exited the object's space
+        if (collider.GetComponent<PlayerController>() != null)
+        {
+            ShouldFadeObject = false;
+        }
+    }
+
+    /// <summary>
+    /// Decrements the alpha value over time until it reaches 0f
+    /// </summary>
+    private void FadeObjectOut()
+    {
+        if (SpriteRendererScript.color.a > 0f)
+        {
+            float alphaValue = SpriteRendererScript.color.a;
+            alphaValue -= Time.deltaTime * FadeSpeed;
+            SpriteRendererScript.color = new Color(SpriteRendererScript.color.r, SpriteRendererScript.color.g, SpriteRendererScript.color.b, alphaValue);
+        }
+    }
+
+    /// <summary>
+    /// Increments the alpha value over time until it reaches 1f
+    /// </summary>
+    private void FadeObjectIn()
+    {
+        if (SpriteRendererScript.color.a < 1f)
+        {
+            float alphaValue = SpriteRendererScript.color.a;
+            alphaValue += Time.deltaTime * FadeSpeed;
+            SpriteRendererScript.color = new Color(SpriteRendererScript.color.r, SpriteRendererScript.color.g, SpriteRendererScript.color.b, alphaValue);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpriteRendererScript = this.gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (ShouldFadeObject)
+        {
+            FadeObjectIn();
+        }
+        else
+        {
+            FadeObjectOut();
+        }
+    }
+}
