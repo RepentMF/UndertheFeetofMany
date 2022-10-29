78a17029d4f393af3637cfe6098584c2c0b13e93
RepentMF <32201016+RepentMF@users.noreply.github.com>
2022-10-29 15:37:01 -0400

    changing a bunch of stuff

--- UtFoM_Project2/Assets/Scripts/ActivePageManager.cs
+++ UtFoM_Project2/Assets/Scripts/ActivePageManager.cs
@@ -21,10 +21,21 @@ public class ActivePageManager : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        DynamicText.text = "HP: " + MainStats.CurrentHealth.ToString() + "/(" + MainStats.MaxHealth.ToString()
+        if (MainInventory.EquippedWeapon != null)
+        {
+            DynamicText.text = "HP: " + MainStats.CurrentHealth.ToString() + "/(" + MainStats.MaxHealth.ToString()
             + "+" + "0" + ")    " + "SP: (" + MainStats.MaxStamina.ToString() + "+" + "0" + ")" + "\n" +
             "MP: " + MainStats.CurrentMana.ToString() + "/(" + MainStats.MaxMana.ToString()+ "+" + "0" +  
             ")    " + "TP: " + "(" + MainStats.MaxTrinket.ToString() + "+" + "0" + ")" + "\n" + "Current Weapon: "
             + MainInventory.EquippedWeapon.Name + "\n" + "Spells: " + "\n" + "Trinkets: " + "\n" + "Current Quest: ";
+        }
+        else
+        {
+            DynamicText.text = "HP: " + MainStats.CurrentHealth.ToString() + "/(" + MainStats.MaxHealth.ToString()
+            + "+" + "0" + ")    " + "SP: (" + MainStats.MaxStamina.ToString() + "+" + "0" + ")" + "\n" +
+            "MP: " + MainStats.CurrentMana.ToString() + "/(" + MainStats.MaxMana.ToString()+ "+" + "0" +  
+            ")    " + "TP: " + "(" + MainStats.MaxTrinket.ToString() + "+" + "0" + ")" + "\n" + "Current Weapon: "
+            + "\n" + "Spells: " + "\n" + "Trinkets: " + "\n" + "Current Quest: ";
+        }
     }
 }
