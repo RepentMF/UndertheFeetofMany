40bd40d5390ceabfd71a8b8545d2085054c17b6a
RepentMF <32201016+RepentMF@users.noreply.github.com>
2023-04-01 08:41:27 -0400

    oopsie

--- UtFoM_Project2/Assets/Scripts/LifebloodParticleManager.cs
+++ UtFoM_Project2/Assets/Scripts/LifebloodParticleManager.cs
@@ -11,7 +11,7 @@ public class LifebloodParticleManager : MonoBehaviour
     {
         EnemyInformation = this.gameObject.GetComponent<EnemyInfo>();
         ParticleSystemScript = this.gameObject.GetComponent<ParticleSystem>();
-        ParticleSystem.Particle[] particles = ParticleSystemScript.Particle[ParticleSystemScript.particleCount];
+        //ParticleSystem.Particle[] particles = ParticleSystem.Particle[ParticleSystemScript.particleCount];
         // ParticleSystem.Particle[] ParticleArray = ParticleSystemScript.Particle[ParticleSystemScript.particleCount];
         // for (int i = 1; i < ParticleSystemScript.particleCount; i++)
         // {
